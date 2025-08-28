cdc6733fa4b091cd8f53d9acbaabc556cfd327ac
carolrossi <anacarollinirossi@gmail.com>
2025-08-28 11:11:04 -0300

    style: responsividade notebook

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,10 +5,9 @@ export default function Home() {
     <>
     <Header />
     <div className='grid grid-cols-2'>
-      <div className='text-white py-40  ms-30'>
+      <div className='text-white lg:py-35  ms-30'>
         <h1 className='font-bold text-5xl max-w-150 mb-12'>AI Your Business Can Run With</h1>
-        <p className=' text-xl '>Grammarly Business combines advanced AI communication assistance with the collective knowledge and style of your organization to help teams communicate more effectively. It provides real-time writing suggestions for clarity, tone, and professionalism—helping ensure that every message reflects your brand and goals.</p>
-        <p className='text-xl font-sans'>Whether you're collaborating on emails, documents, or messages, Grammarly Business works where your team already does—like Google Docs, Slack, Outlook, and more. Experience smarter, faster communication with the intelligence you need, exactly when and where you need it.</p>
+        <p className='text-xl font-sans'>Grammarly Business combines advanced AI communication assistance with the collective knowledge and style of your organization to help teams communicate more effectively. It provides real-time writing suggestions for clarity, tone, and professionalism—helping ensure that every message reflects your brand and goals.</p>
 
         <button className="bg-white hover:bg-gray-500 text-black rounded-md p-4 cursor-pointer w-50 transition-all duration-200 mt-10 font-sans">Start a free trial</button>
         
